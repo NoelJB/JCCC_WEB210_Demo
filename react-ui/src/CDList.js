b0702beeae2dbacdf3bfc74e4e696d121fc7c086
Noel J. Bergman <noel@devtech.com>
2025-05-28 22:13:52 -0400

    Fix formatting

--- react-ui/src/CDList.js
+++ react-ui/src/CDList.js
@@ -1,39 +1,39 @@
 import { Fragment } from "react"
 import { Link, Outlet } from "react-router-dom"
 
-function CDList({compactDiscs}) {
+function CDList({ compactDiscs }) {
     return (
         <>
-			<section id="cd-list">
-				<h1>Available CDs</h1>
-				<table>
-					<thead>
-						<tr>
-							<th>Artist</th>
-							<th>Title</th>
-							<th>Delete</th>
-							<th>Edit</th>
-						</tr>
-					</thead>
-					<tbody id="cd-items">
-						{
-							compactDiscs.map(cd => {
-								const path = `/cds/${cd.id}`
-								return (
-									<Fragment key={cd.title}>
-										<tr>
-											<td>{cd.artist}</td>
-											<td><Link to={path}>{cd.title}</Link></td>
-											<td><button delete data-cd_id="{cd.id}">&#128465;</button></td>
-											<td><button edit data-cd_id="{cd.id}">&#x270e;</button></td>
-										</tr>
-									</Fragment>
-								)
-							})
-						}
-					</tbody>
-				</table>
-			</section>
+            <section id="cd-list">
+                <h1>Available CDs</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Artist</th>
+                            <th>Title</th>
+                            <th>Delete</th>
+                            <th>Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody id="cd-items">
+                        {
+                            compactDiscs.map(cd => {
+                                const path = `/cds/${cd.id}`
+                                return (
+                                    <Fragment key={cd.title}>
+                                        <tr>
+                                            <td>{cd.artist}</td>
+                                            <td><Link to={path}>{cd.title}</Link></td>
+                                            <td><button delete data-cd_id="{cd.id}">&#128465;</button></td>
+                                            <td><button edit data-cd_id="{cd.id}">&#x270e;</button></td>
+                                        </tr>
+                                    </Fragment>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </section>
             <Outlet />
         </>
     )
